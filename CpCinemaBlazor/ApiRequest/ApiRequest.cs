d705a29c6384f6f3658125b36e049057ab85bab8
Yulia Karelova <j.karelowa@gmail.com>
2025-02-20 12:52:37 +0300

    update

--- CpCinemaBlazor/ApiRequest/ApiRequest.cs
+++ CpCinemaBlazor/ApiRequest/ApiRequest.cs
@@ -80,23 +80,28 @@ namespace CpCinemaBlazor.ApiRequest
             }
         }
 
-        public async void CreateUserLoginAsync(AddUser user)
+        public async Task<string> CreateUserLoginAsync(AddUser user)
         {
-            try
-            {
-                var json = JsonConvert.SerializeObject(user);
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsJsonAsync($"http://localhost:5005/api/UsersLogins/CreateNewUserAndLogin", user);
 
-                HttpResponseMessage response = await _httpClient.PostAsync($"http://localhost:5005/api/UsersLogins/CreateNewUserAndLogin", content);
-            }
-            catch (Exception ex)
+            if (!response.IsSuccessStatusCode)
             {
-                _logger.LogError(ex, "Ошибка при запросе");
+                var errorContent = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Authorization failed: {errorContent}");
             }
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true, // Игнорировать регистр свойств
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase // Сопоставлять snake_case с CamelCase
+            };
+            var tokenResponse = JsonSerializer.Deserialize<TokenResponse>(responseContent, options);
+            var token = tokenResponse.Token;
+
+            return token;
         }
-
-        //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoi0JrQsNGC0Y8iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJVU0VSIiwibmJmIjoxNzM3NzMyNDUzLCJleHAiOjE3Mzc3MzQyNTMsImlzcyI6IkFQSVNlcnZlciIsImF1ZCI6IkJsYXpvckFwcCJ9.X-NkXeMxrG-EujaV4YzdpliXtXAiY16JckbOWdrgEqw
-
         
         public async Task<string> AuthorizationAsync(AuthUser auth)
         {
